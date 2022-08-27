c099503189565f441fe9a8174995d6e83c1fd147
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-08-27 16:09:02 +0800

    Update owner-bc.js

--- plugins/owner-bc.js
+++ plugins/owner-bc.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, text }) => {
   let chats = Object.keys(await conn.chats)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
   for (let id of chats) {
-       let bcbg = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg'
+       let bcbg = 'https://telegra.ph/file/989bd78701f27eec3293a.png'
        await conn.send2ButtonImg(id, bcbg, text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner')
      }
   m.reply('*Broadcast selesai*')
